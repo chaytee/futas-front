8075bfaa335c7ec58f3c62dafcfa0607da776ba4
chaytee <shimashima.works@gmail.com>
2022-02-19 08:16:00 +0900

    websoket  修正

--- pages/chatRoom.vue
+++ pages/chatRoom.vue
@@ -76,7 +76,7 @@ export default {
   mounted() {
     /* こちらエラーになる wss://futasu-v1-api.herokuapp.com/cable */
     /* ws://localhost:3000/cable */
-    const cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+    const cable = ActionCable.createConsumer("wss://futasu-v1-api.herokuapp.com/cable");
     this.messageChannel = cable.subscriptions.create("RoomChannel", {
       connected: () => {
         this.getMessages().then(() => {
